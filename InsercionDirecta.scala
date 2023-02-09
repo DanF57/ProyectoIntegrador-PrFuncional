82e56689132d4ad07fb5f6dc9fed71c3789a9144
Daniel Eduardo Flores <danser2002@hotmail.com>
2023-02-09 15:14:03 -0500

    Correcion Version Directa

--- InsercionDirecta.scala
+++ InsercionDirecta.scala
@@ -23,7 +23,7 @@ object InsercionDirecta extends App {
   reader.close()
 
   //-----------------------------------------------------Entidades-----------------------------------------------------
-  /*
+
   //-------------Tabla OriLanguage-------------
   val originalLanguage = data
     .flatMap(elem => elem.get("original_language"))
@@ -45,7 +45,7 @@ object InsercionDirecta extends App {
                     budget: Long,
                     homepage: String,
                     keywords: String,
-                    idOriginalLanguage: Int,
+                   original_language: String,
                     original_title: String,
                     overview: String,
                     popularity: Double,
@@ -69,45 +69,7 @@ object InsercionDirecta extends App {
       },
       row("homepage"),
       row("keywords"),
-      row("original_language") match {
-        case x if x == "en" => 1
-        case x if x == "ja" => 2
-        case x if x == "fr" => 3
-        case x if x == "zh" => 4
-        case x if x == "es" => 5
-        case x if x == "de" => 6
-        case x if x == "hi" => 7
-        case x if x == "ru" => 8
-        case x if x == "ko" => 9
-        case x if x == "te" => 10
-        case x if x == "cn" => 11
-        case x if x == "it" => 12
-        case x if x == "nl" => 13
-        case x if x == "ta" => 14
-        case x if x == "sv" => 15
-        case x if x == "th" => 16
-        case x if x == "da" => 17
-        case x if x == "xx" => 18
-        case x if x == "hu" => 19
-        case x if x == "cs" => 20
-        case x if x == "pt" => 21
-        case x if x == "is" => 22
-        case x if x == "tr" => 23
-        case x if x == "nb" => 24
-        case x if x == "af" => 25
-        case x if x == "pl" => 26
-        case x if x == "he" => 27
-        case x if x == "ar" => 28
-        case x if x == "vi" => 29
-        case x if x == "ky" => 30
-        case x if x == "id" => 31
-        case x if x == "ro" => 32
-        case x if x == "fa" => 33
-        case x if x == "no" => 34
-        case x if x == "sl" => 35
-        case x if x == "ps" => 36
-        case x if x == "el" => 37
-      },
+      row("original_language"),
       row("original_title"),
       row("overview"),
       row("popularity") match {
@@ -144,10 +106,10 @@ object InsercionDirecta extends App {
 
   val movies = movieData.map(x =>
     sql"""
-      INSERT INTO Movie(idMovie, `index`, budget, homepage, keywords, idOrigLang, original_title, overview, popularity,
+      INSERT INTO Movie(idMovie, budget, homepage, keywords, idOrigLang, original_title, overview, popularity,
       release_date, revenue, runtime, idStatus, tagline, title, vote_average, vote_count)
       VALUES
-      (${x.idMovie}, ${x.indedx}, ${x.budget}, ${x.homepage},${x.keywords}, ${x.idOriginalLanguage}, ${x.original_title}, ${x.overview},
+      (${x.idMovie}, ${x.budget}, ${x.homepage},${x.keywords}, ${x.original_language}, ${x.original_title}, ${x.overview},
       ${x.popularity}, ${x.release_date}, ${x.revenue}, ${x.runtime},${x.idStatus}, ${x.tagline},
       ${x.title}, ${x.vote_average}, ${x.vote_count})
     """.stripMargin
@@ -179,29 +141,6 @@ object InsercionDirecta extends App {
     .filter(_._2.nonEmpty)
     .map(x => (x._1.toInt, x._2.split(" ").toList))
     .flatMap(x => x._2.map((x._1, _)))
-    .map(x => (x._1, x._2 match {
-      case x if x == "Action" => 1
-      case x if x == "Adventure" => 2
-      case x if x == "Fantasy" => 3
-      case x if x == "Science-Fiction" => 4
-      case x if x == "Crime" => 5
-      case x if x == "Drama" => 6
-      case x if x == "Thriller" => 7
-      case x if x == "Animation" => 8
-      case x if x == "Family" => 9
-      case x if x == "Western" => 10
-      case x if x == "Comedy" => 11
-      case x if x == "Romance" => 12
-      case x if x == "Horror" => 13
-      case x if x == "Mystery" => 14
-      case x if x == "History" => 15
-      case x if x == "War" => 16
-      case x if x == "Music" => 17
-      case x if x == "Documentary" => 18
-      case x if x == "Foreign" => 19
-      case x if x == "TV" => 20
-      case x if x == "Movie" => 21
-    }))
 
   val movie_genres = movieGenres.map(x =>
     sql"""
@@ -244,7 +183,7 @@ object InsercionDirecta extends App {
       .update
       .apply())
 
-*/
+
   //-------------Tabla Movie_Languages------------
   val movieLanguages = data
     .map(row => (row("id"), Json.parse(row("spoken_languages"))))
@@ -272,20 +211,5 @@ object InsercionDirecta extends App {
       txtOr = txtOr.replace(textOriginal, replacementText)
     }
     txtOr
-  }
-
-  def names(dataRaw: String): Option[String] = {
-    val response: Response = requests
-      .post("http://api.meaningcloud.com/topics-2.0",
-        data = Map("key" -> "903292c5e69738082bea48a1b22c3865",
-          "lang" -> "en",
-          "txt" -> dataRaw,
-          "tt" -> "e"),
-        headers = Map("content-type" -> "application/x-www-form-urlencoded"))
-    Thread.sleep(500)
-    if (response.statusCode == 200) {
-      Option(response.text)
-    } else
-      Option.empty
   }
 }
